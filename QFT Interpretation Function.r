f762a911c0e321fcbafe0ce88132883d526a3c72
Matt Parker <matt.parker@dhha.org>
2011-02-04 17:50:38 -0700

    Convert to Python-style commenting.
    It looks better.  Just add a space after the hash.

--- QFT Interpretation Function.r
+++ QFT Interpretation Function.r
@@ -1,36 +1,36 @@
-#This function takes values of nil tube, TB tube, and mitogen scores, and returns Positive, Negative, or Indeterminate.
+# This function takes values of nil tube, TB tube, and mitogen scores, and returns Positive, Negative, or Indeterminate.
 qft.interp <- function(nil, tb, mitogen, tbnil.cutoff = 0.35){
-#Given vectors of nil, TB antigen, and mitogen results in IU/mL,
-#this function computes QFT qualitative interpretations.  The function
-#uses the Cellestis North America criterion by default;
-#alternative TB - nil cutoffs can be specified using the tbnil.cutoff
-#argument.  Further changes to the criteria require modification of the
-#function.
+# Given vectors of nil, TB antigen, and mitogen results in IU/mL,
+# this function computes QFT qualitative interpretations.  The function
+# uses the Cellestis North America criterion by default;
+# alternative TB - nil cutoffs can be specified using the tbnil.cutoff
+# argument.  Further changes to the criteria require modification of the
+# function.
 
-    #Check for equal vector lengths
+    # Check for equal vector lengths
     if(!isTRUE(all.equal(length(nil), length(tb))) |
        !isTRUE(all.equal(length(nil), length(mitogen)))){stop(
            "The vectors of TB, nil, and mitogen values must all be the same length.")}
     
-    #Check for numeric results
+    # Check for numeric results
     if(!isTRUE(is.numeric(nil) &
                is.numeric(tb) &
                is.numeric(mitogen))){stop(
                "The vectors of TB, nil, and mitogen values must all be numeric.")}
     
-    #Set up the results vector
+    # Set up the results vector
     result <- rep(NA, times = length(nil)) 
 
-    #The floating point comparison will bite you in the ass for some of these.
-    #Instead of >=, define a small value and add it to the number being compared
-    #In essence, convert any left-hand value that's truly equal to the right-hand
-    # value into one that is *greater* than the right-hand value.
-    #Set an epsilon that is tiny relative to the numbers being compared.
+    # The floating point comparison will bite you in the ass for some of these.
+    # Instead of >=, define a small value and add it to the number being compared
+    # In essence, convert any left-hand value that's truly equal to the right-hand
+    #  value into one that is *greater* than the right-hand value.
+    # Set an epsilon that is tiny relative to the numbers being compared.
     epsilon <- 1e-10
 
-    #Iterate through each test.
+    # Iterate through each test.
     for(i in seq_along(result)){
-        #If any test value is NA, no interpretation - skip
+        # If any test value is NA, no interpretation - skip
         if(is.na(tb[i]) | is.na(nil[i]) | is.na(mitogen[i])) {next}
 
         if(nil[i] + epsilon > 8.0) {result[i] <- "Indeterminate"} else {
@@ -52,4 +52,4 @@ qft.interp <- function(nil, tb, mitogen, tbnil.cutoff = 0.35){
 
 
 
-#Development notes: This algorithm is based on the flow chart L05995008A Apr 2007 from Cellestis.
+# Development notes: This algorithm is based on the flow chart L05995008A Apr 2007 from Cellestis.
--- TSPOT Interpretation Function.r
+++ TSPOT Interpretation Function.r
@@ -1,15 +1,15 @@
-#This function takes values of nil tube, Panel A and Panel B, PHA/mitogen, and returns
-#Positive, Negative, Indeterminate, or Failed.
+# This function takes values of nil tube, Panel A and Panel B, PHA/mitogen, and returns
+# Positive, Negative, Indeterminate, or Failed.
 tspot.interp <- function(nil, panel_a, panel_b, mitogen){
-    #Check for equal vector lengths
+    # Check for equal vector lengths
     if(!isTRUE(all.equal(length(nil), length(panel_a), length(panel_b), length(mitogen)))){stop("The vectors of nil, Panel A, Panel B, and mitogen values must all be the same length.")}
     
-    #Setup the results vector
+    # Setup the results vector
     result <- rep(NA, times = length(nil)) 
 
-    #Iterate through each test.
+    # Iterate through each test.
     for(i in seq_along(result)){
-        #If any test value is NA, no interpretation - skip
+        # If any test value is NA, no interpretation - skip
         if(is.na(panel_a[i]) | is.na(panel_b[i]) | is.na(nil[i]) | is.na(mitogen[i])) {next}
 
         if(nil[i] > 10){result[i] <- "Invalid - high nil"} else {
@@ -36,12 +36,12 @@ tspot.interp <- function(nil, panel_a, panel_b, mitogen){
 }
 
 
-#Development note: This algorithm is based off of the TSPOT.TB Package Insert from 
-#Oxford Immunotec (document ID looks to be PI-TB-US-V1).  
+# Development note: This algorithm is based off of the TSPOT.TB Package Insert from 
+# Oxford Immunotec (document ID looks to be PI-TB-US-V1).  
 
 
-#Write a test case sometime.     
-#Possible values for things: NA, 1:21, text, factor elements, very high numbers,
-#negative numbers...              
+# Write a test case sometime.     
+# Possible values for things: NA, 1:21, text, factor elements, very high numbers,
+# negative numbers...              
   
 
