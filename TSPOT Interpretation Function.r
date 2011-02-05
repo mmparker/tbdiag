120e0d84dc4062c501ff6f1ae7c293beca61dc46
Matt Parker <matt.parker@dhha.org>
2011-02-04 17:56:03 -0700

    Nice comment and numeric vector check for TSPOT.

--- TSPOT Interpretation Function.r
+++ TSPOT Interpretation Function.r
@@ -1,11 +1,24 @@
-# This function takes values of nil tube, Panel A and Panel B, PHA/mitogen, and returns
-# Positive, Negative, Indeterminate, or Failed.
+
 tspot.interp <- function(nil, panel_a, panel_b, mitogen){
+# Given vectors of nil, TB antigen (panels A and B), and mitogen results in spots,
+# this function computes TSPOT qualitative interpretations.  The function
+# uses the Oxford Immunotec North America criterion by default;
+# alternative TB - nil cutoffs can be specified using the tbnil.cutoff
+# argument.  Further changes to the criteria require modification of the
+# function.
+
     # Check for equal vector lengths
     if(!isTRUE(all.equal(length(nil), length(panel_a)) |
                all.equal(length(nil), length(panel_b)) |
                all.equal(length(nil), length(mitogen)))){stop(
                "The vectors of nil, Panel A, Panel B, and mitogen values must all be the same length.")}
+    
+    # Check for numeric results
+    if(!isTRUE(is.numeric(nil) &
+               is.numeric(panel_a) &
+               is.numeric(panel_b) &
+               is.numeric(mitogen))){stop(
+               "The vectors of nil, TB - Panel A, TB - Panel B, and mitogen values must all be numeric.")}
     
     # Setup the results vector
     result <- rep(NA, times = length(nil)) 
