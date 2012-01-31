d22def1bdc9e62eea904f0b440d1208fcc735c67
Matt Parker <mattmparker@gmail.com>
2012-01-30 22:36:28 -0700

    Add the global criteria with no borderline.

--- tbdiag/R/tspot.interp.R
+++ tbdiag/R/tspot.interp.R
@@ -126,6 +126,40 @@ tspot.criteria.oxford.usa <- function(interp.this){
 }
 
 
+tspot.criteria.oxford.global <- function(interp.this){
+
+    
+    # Set up the results vector
+    result <- rep(NA, times = length(nil)) 
+
+    # Identify the maximum of Panel A - Nil and Panel B - Nil
+    panel.max <- ifelse((panel_a - nil) > (panel_b - nil),
+                        yes = (panel_a - nil), no = (panel_b - nil)
+    )
+
+    # Compute the results
+    result[is.na(result) &
+           nil > 10] <- "Invalid - high nil"
+
+    result[is.na(result) &
+           panel.max %in% c(5, 6, 7)] <- "Borderline"
+
+    result[is.na(result) &
+           panel.max >= 6] <- "Positive"
+
+    result[is.na(result) &
+           panel.max <= 5 &
+           mitogen >= 20] <- "Negative"
+
+    result[is.na(result) &
+           panel.max <= 4 &
+           mitogen < 20] <- "Invalid - low mitogen"
+
+    return(result)
+}
+
+
+
 
 
 # Function to check for integerness of spots - see ?is.integer for explanation
