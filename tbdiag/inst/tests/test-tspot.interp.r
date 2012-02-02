e00a86334938b4d4f0bc8e4b0ca5be4ee617e69a
Matt Parker <mattmparker@gmail.com>
2012-02-01 19:15:19 -0700

    Update tests to reflect standalone trim.output fun

--- tbdiag/inst/tests/test-tspot.interp.r
+++ tbdiag/inst/tests/test-tspot.interp.r
@@ -135,38 +135,6 @@ expect_that(tspot.cens(20 + tol), equals(20))
 })
 
 
-
-################################################################################
-# trim.tspot.output defaults to terse when an invalid verbosity argument is passed
-test_that("trim.tspot.output defaults to terse and gives a warning when an invalid verbosity argument is passed", {
-
-expect_that(trim.tspot.output("Positive dood!"), equals("Positive"))
-expect_that(trim.tspot.output("Positive dood!", "loquacious"), equals("Positive"))
-expect_that(trim.tspot.output("Positive dood!", "1"), equals("Positive"))
-expect_that(trim.tspot.output("Positive dood!", NA), equals("Positive"))
-
-# trim.tspot.output warns when an invalid verbosity argument is passed
-expect_that(trim.tspot.output("Positive dood!", "loquacious"), gives_warning())
-expect_that(trim.tspot.output("Positive dood!", "1"), gives_warning())
-expect_that(trim.tspot.output("Positive dood!", NA), gives_warning())
-
-})
-
-
-################################################################################
-# trim.tspot.output returns appropriate result permutations
-test_that("trim.tspot.output returns appropriate result permutations", {
-
-expect_that(trim.tspot.output("Positive dood!"), equals("Positive"))
-expect_that(trim.tspot.output("Positive dood!", "onechar"), equals("P"))
-expect_that(trim.tspot.output("Positive dood!", "terse"), equals("Positive"))
-expect_that(trim.tspot.output("Positive dood!", "verbose"), 
-            equals("Positive dood!"))
-expect_that(trim.tspot.output("Positive dood!", "loquacious"), equals("Positive"))
-
-})
-
-
 ################################################################################
 # tspot.interp returns a vector of acceptable results
 test_that("tspot.interp returns a vector of acceptable results", {
