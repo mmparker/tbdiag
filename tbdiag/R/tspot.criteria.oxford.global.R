50e7d060f96e721f00c2ae75eff2975a468ba755
Matt Parker <mattmparker@gmail.com>
2012-02-01 19:27:42 -0700

    tspot.criteria funs refer to tspot.obj from tspot.interp now

--- tbdiag/R/tspot.criteria.oxford.global.R
+++ tbdiag/R/tspot.criteria.oxford.global.R
@@ -3,16 +3,17 @@ tspot.criteria.oxford.global <- function(tspot.obj){
 
     
     # Set up the results vector
-    result <- rep(NA, times = length(nil)) 
+    result <- rep(NA, times = length(tspot.obj&nil)) 
 
     # Identify the maximum of Panel A - Nil and Panel B - Nil
-    panel.max <- ifelse((panel_a - nil) > (panel_b - nil),
-                        yes = (panel_a - nil), no = (panel_b - nil)
+    panel.max <- ifelse((panel_a - tspot.obj$nil) > (panel_b - tspot.obj$nil),
+                        yes = (panel_a - tspot.obj$nil), 
+                        no = (panel_b - tspot.obj$nil)
     )
 
     # Compute the results
     result[is.na(result) &
-           nil > 10] <- "Invalid - high nil"
+           tspot.obj$nil > 10] <- "Invalid - high nil"
 
     result[is.na(result) &
            panel.max %in% c(5, 6, 7)] <- "Borderline"
