639615a9666c607558b4c130c689e0e2a59d7380
Matt Parker <mattmparker@gmail.com>
2012-02-02 16:48:36 -0700

    devtools can't seem to find packages - give it a path

--- test_tbdiag.r
+++ test_tbdiag.r
@@ -4,7 +4,7 @@ library(devtools)
 library(testthat)
 
 # Load the tbdiag code
-load_all("tbdiag", reset = TRUE)
+load_all("I:/USERS/MParker/Rpackages/tbdiag/tbdiag", reset = TRUE)
 
 # Run all tests in inst/tests/
 test("tbdiag")
