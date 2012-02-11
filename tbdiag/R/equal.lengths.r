0e83d1b9b0b08d000edfc1f56098c66a0a5e4384
Matt Parker <mattmparker@gmail.com>
2012-02-10 17:16:26 -0700

    Add some data checks to equal.lengths(), and create its tests.

--- tbdiag/R/equal.lengths.r
+++ tbdiag/R/equal.lengths.r
@@ -5,6 +5,14 @@
 equal.lengths <- function(...){
 
     vec.list <- list(...)
+
+    # Test that at least one vector was supplied
+    if(length(vec.list) < 1){stop("No vectors supplied.")}
+
+    # Warn if only one vector was supplied
+    if(length(vec.list) == 1){warning("Only one vector supplied - no basis for comparison of vector lengths.")}
+
+    # Throw error if vectors are of unequal lengths
     if(!all(sapply(vec.list, length) == length(vec.list[[1]]))){
         stop("All input vectors must be the same length.")
     }
