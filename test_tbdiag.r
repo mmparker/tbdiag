0449e0e0c549f6ff024190c69a36e761a90302fa
Matt Parker <mattmparker@gmail.com>
2012-06-06 16:03:03 -0600

    Add roxygenize() to the test file.

--- test_tbdiag.r
+++ test_tbdiag.r
@@ -2,12 +2,16 @@
 
 library(devtools)
 library(testthat)
+library(roxygen2)
+
+pkg.path <- "I:/USERS/MParker/Rpackages/tbdiag/tbdiag"
 
 # Load the tbdiag code
-load_all("I:/USERS/MParker/Rpackages/tbdiag/tbdiag", reset = TRUE)
+load_all(pkg.path, reset = TRUE)
 
 # Run all tests in inst/tests/
-test("I:/USERS/MParker/Rpackages/tbdiag/tbdiag")
+test(pkg.path)
 
 # Run roxygen to update documentation
 # document("tbdiag")
+roxygenize(pkg.path)
