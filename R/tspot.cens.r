d20ce5c92f06c8443bd9de25d6cdc3bb80970a56
Matt Parker <mattmparker@gmail.com>
2013-06-26 16:43:42 -0600

    Add docs for non-exported functions, just in case.

--- R/tspot.cens.r
+++ R/tspot.cens.r
@@ -1,6 +1,20 @@
 # This function censors TSPOT result values greater than 20 spots to 20 spots,
 # per Oxford Immunotec's instructions
 
+#' Censor TSPOT quantitative results to 20 spots.
+#'
+#' This functions censors a vector of quantitative results of the 
+#' TSPOT (nil, Panel A, Panel B, mitogen) from counts greater than 20 spots to
+#' 20 spots, following the Oxford test insert instructions.
+#' Intended for internal use only.
+#' 
+#' @param x A vector of numeric results from the TSPOT
+#'
+#' @return The input vector with all results greater than 20 censored to 20.
+#' 
+#' @seealso \code{\link{qft.cens}}
+#' 
+
 tspot.cens <- function(x){
     if(any(x > 20, na.rm = TRUE)){
 
